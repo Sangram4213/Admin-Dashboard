851c5d5bfda0d109547fb9877336fb2a8f99040b
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-04-22 23:45:26 +0530

    add all the tables in dashboard,product and trasactions

--- src/components/TableHOC.tsx
+++ src/components/TableHOC.tsx
@@ -1,24 +1,44 @@
-import { useTable, Column, TableOptions } from "react-table";
+import {
+  useTable,
+  Column,
+  TableOptions,
+  useSortBy,
+  usePagination,
+} from "react-table";
+import {
+  AiOutlineSortAscending,
+  AiOutlineSortDescending,
+} from "react-icons/ai";
 
-type temp = {
-  status: string;
-  id: string;
-  amount: number;
-};
 function TableHOC<T extends Object>(
   columns: Column<T>[],
   data: T[],
   containerClassname: string,
-  heading: string
+  heading: string,
+  showPagination: boolean = false
 ) {
   return function HOC() {
     const options: TableOptions<T> = {
       columns,
       data,
+      initialState: {
+        pageSize: 6,
+      },
     };
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-      useTable(options);
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      page,
+      prepareRow,
+      nextPage,
+      previousPage,
+      canNextPage,
+      canPreviousPage,
+      pageCount,
+      state:{pageIndex},
+    } = useTable(options, useSortBy, usePagination);
 
     return (
       <div className={containerClassname}>
@@ -29,28 +49,46 @@ function TableHOC<T extends Object>(
             {headerGroups.map((headerGroup) => (
               <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                     {column.render("Header")}
+                    {column.isSorted && (
+                      <span>
+                        {column.isSortedDesc ? (
+                          <AiOutlineSortDescending />
+                        ) : (
+                          <AiOutlineSortAscending />
+                        )}
+                      </span>
+                    )}
                   </th>
                 ))}
               </tr>
             ))}
           </thead>
           <tbody {...getTableBodyProps()}>
-                 {
-                    rows.map((row)=>{
-                        prepareRow(row);
-                        return <tr{...row.getRowProps()}>
-                        {row.cells.map(cell=>(
-                            <td {...cell.getCellProps()}>
-                               {cell.render("Cell")}
-                            </td>
-                        ))}
-                        </tr>
-                    })
-                 }
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  ))}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
+        {showPagination && (
+          <div className="table-pagination">
+            <button disabled={!canPreviousPage} onClick={previousPage}>
+              Prev
+            </button>
+            <span>{`1 of ${pageCount}`}</span>
+            <button disabled={!canNextPage} onClick={nextPage}>
+              Next
+            </button>
+          </div>
+        )}
       </div>
     );
   };
--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,10 +1,108 @@
+import {ReactElement} from 'react';
 import AdminSideBar from "../components/AdminSideBar";
+import { Column } from "react-table";
+import {useState,useCallback} from 'react';
+import TableHOC from '../components/TableHOC';
+import { FaTrash } from 'react-icons/fa';
+
+type DataType ={
+  avatar:ReactElement,
+  name:string,
+  email:string,
+  gender:string,
+  role:string,
+  action: ReactElement
+}
+
+const columns: Column<DataType>[] = [
+  {
+    Header: "Avatar",
+    accessor: "avatar",
+  },
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Gender",
+    accessor: "gender",
+  },
+  {
+    Header: "Role",
+    accessor: "role",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+  },
+];
+
+const img = "https://randomuser.me/api/portraits/women/54.jpg";
+const img2 = "https://randomuser.me/api/portraits/women/50.jpg";
+
+const arr: DataType[] = [
+  {
+    avatar: (
+      <img
+        style={{
+          borderRadius: "50%",
+        }}
+        src={img}
+        alt="Shoes"
+      />
+    ),
+    name: "Emily Palmer",
+    email: "emily.palmer@example.com",
+    gender: "female",
+    role: "user",
+    action: (
+      <button>
+        <FaTrash />
+      </button>
+    ),
+  },
+
+  {
+    avatar: (
+      <img
+        style={{
+          borderRadius: "50%",
+        }}
+        src={img2}
+        alt="Shoes"
+      />
+    ),
+    name: "May Scoot",
+    email: "aunt.may@example.com",
+    gender: "female",
+    role: "user",
+    action: (
+      <button>
+        <FaTrash />
+      </button>
+    ),
+  },
+];
 
 const Customers = () => {
+
+  const [data] = useState<DataType[]>(arr);
+
+  const Table = useCallback(
+    TableHOC<DataType>(
+      columns,
+      data,
+      "dashboard-product-box",
+      "Customers",
+      true
+    ),
+    []
+  );
+
   return (
     <div className="admin-container">
         <AdminSideBar/>
-        <main>asdsd</main>
+        <main>{Table()}</main>
     </div>
   )
 }
