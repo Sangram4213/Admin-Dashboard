60064c6302fe02b5c38a23c78508ab98f9b53727
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-04-26 18:20:16 +0530

    able to create coupon functionality

--- src/pages/apps/Coupon.tsx
+++ src/pages/apps/Coupon.tsx
@@ -1,12 +1,113 @@
-import AdminSideBar from "../../components/AdminSideBar"
+import { useState, FormEvent, useEffect } from "react";
+import AdminSideBar from "../../components/AdminSideBar";
+
+const allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const allNumbers = "1234567890";
+const allSymbols = "!@#$%^&*()_+";
 
 const Coupon = () => {
+  const [size, setSize] = useState<number>(8);
+  const [prefix, setPrefix] = useState<string>("");
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
+  const [includeCharacters, setIncludeCharacters] = useState<boolean>(false);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const [coupon, setCoupon] = useState<string>("");
+
+  useEffect(() => {
+    setIsCopied(false);
+  }, [coupon]);
+
+  const copyText = async (coupon: string) => {
+    await window.navigator.clipboard.writeText(coupon);
+    setIsCopied(true);
+  };
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!includeNumbers && !includeCharacters && !includeSymbols)
+      return alert("Please Select One At Least");
+
+    let result: string = prefix || "";
+    const loopLength: number = size - result.length;
+
+    for (let i = 0; i < loopLength; i++) {
+      let entireString: string = "";
+      if (includeCharacters) entireString += allLetters;
+      if (includeNumbers) entireString += allNumbers;
+      if (includeSymbols) entireString += allSymbols;
+
+      const randomNum: number = ~~(Math.random() * entireString.length);
+      result += entireString[randomNum];
+    }
+
+    setCoupon(result);
+  };
+
   return (
     <div className="admin-container">
-    <AdminSideBar />
-    <main className="dashboard-app-container"></main>
-    </div>
-  )
-}
+      <AdminSideBar />
+      <main className="dashboard-app-container">
+        <h1>Coupon</h1>
+        <section>
+          <form className="coupon-form" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Text to include"
+              value={prefix}
+              onChange={(e) => setPrefix(e.target.value)}
+              maxLength={8}
+            />
 
-export default Coupon
+            <input
+              type="number"
+              placeholder="Coupon Length"
+              value={size}
+              onChange={(e) => setSize(Number(e.target.value))}
+              min={8}
+              max={25}
+            />
+
+            <fieldset>
+              <legend>Include</legend>
+              <input
+                type="checkbox"
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers((prev) => !prev)}
+              />
+              <span>Numbers</span>
+
+              <input
+                type="checkbox"
+                checked={includeCharacters}
+                onChange={() => setIncludeCharacters((prev) => !prev)}
+              />
+              <span>Characters</span>
+
+              <input
+                type="checkbox"
+                checked={includeSymbols}
+                onChange={() => setIncludeSymbols((prev) => !prev)}
+              />
+              <span>Symbol</span>
+            </fieldset>
+            <button type="submit">Generate</button>
+          </form>
+
+          {coupon && (
+            <code>
+              {coupon}{" "}
+              <span onClick={() => copyText(coupon)}>
+                {isCopied ? "Copied" : "Copy"}
+              </span>
+            </code>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Coupon;
